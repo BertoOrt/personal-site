7169296b4e9b13c237c65d9363e07a9846881655
Roberto Ortega <berto.ort@gmail.com>
2015-08-22 16:13:52 -0600

    project wheel

--- js/app.js
+++ js/app.js
@@ -2,19 +2,31 @@ var app = angular.module('app', ['ngAnimate', 'ngRoute']);
 
 $(document).ready(function () {
   setTimeout(function () {
-    $(".test").slideDown(1500);
+    $(".image").slideDown(1500);
   },1000)
 
   $( "#everyone" ).click(function() {
-    $( ".test" ).slideUp(1000);
+    $( ".image" ).slideUp(1000);
   });
   $('.logos').fadeIn(1000)
 
-  $('.test').mousemove(function(e){
+  $('.image').mousemove(function(e){
   var amountMovedX = (e.pageX * - (1 / 80));
   var amountMovedY = (e.pageY * - (1 / 80));
   $(this).css('background-position', amountMovedX + 'px ' + amountMovedY + 'px');
   });
+
+  $('#developer').click(function () {
+    window.location.replace("http://bertoort.com");
+  })
+
+  $('#image').click(function () {
+
+  })
+
+  $('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
 })
 
 // app.config(function($routeProvider, $locationProvider) {
