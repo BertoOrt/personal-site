dfc674f6fd193a0e323e03e24944e672071f2ae9
Roberto Ortega <berto.ort@gmail.com>
2015-08-22 12:58:58 -0600

    intro effects

--- js/app.js
+++ js/app.js
@@ -1,27 +1,28 @@
 var app = angular.module('app', ['ngAnimate', 'ngRoute']);
 
-app.config(function($routeProvider, $locationProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'partials/home.html',
-        controller: 'cartoons'
-      })
-      .when('/about', {
-        templateUrl: 'partials/about.html',
-        controller: 'about'
-      })
-      .when('/find', {
-        templateUrl: 'partials/find.html',
-        controller: 'find'
-      })
-      .when('/math/:factor/:number/:otherNumber', {
-        templateUrl: 'partials/math.html',
-        controller: 'math'
-      })
-      .when('/math/:factor', {
-        templateUrl: 'partials/math.html',
-        controller: 'math'
-      })
-      .otherwise({redirectTo: '/'});
-      $locationProvider.html5Mode(true);
-});
+$(document).ready(function () {
+  setTimeout(function () {
+    $(".test").slideDown(1500);
+  },1000)
+
+  $( "#everyone" ).click(function() {
+    $( ".test" ).slideUp(1000);
+  });
+  $('.logos').fadeIn(1000)
+
+  $('.test').mousemove(function(e){
+  var amountMovedX = (e.pageX * - (1 / 80));
+  var amountMovedY = (e.pageY * - (1 / 80));
+  $(this).css('background-position', amountMovedX + 'px ' + amountMovedY + 'px');
+  });
+})
+
+// app.config(function($routeProvider, $locationProvider) {
+//     $routeProvider
+//       // .when('/', {
+//       //   templateUrl: 'partials/home.html',
+//       //   controller: 'cartoons'
+//       // })
+//       .otherwise({redirectTo: '/'});
+//       $locationProvider.html5Mode(true);
+// });
