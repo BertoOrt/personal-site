6d2e633c83380f63803310558d8304e40d791ff0
Roberto Ortega <berto.ort@gmail.com>
2015-08-25 19:09:46 -0600

    jeff help

---
+++ dist/controllers-15356c3e38.js
@@ -0,0 +1,16 @@
+app.controller('index', [ '$scope', function ($scope) {
+  $scope.regular = false;
+  $scope.d = null;
+  $scope.everyone = function () {
+    $scope.regular = true;
+  }
+  $scope.description = function (tab) {
+    if ($scope.d && $scope.d[tab] === true) {
+      $scope.d = null;
+    } else {
+      $scope.d = null;
+      $scope.d = {};
+      $scope.d[tab] = true;
+    }
+  }
+}])
--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,49 @@
 var gulp = require('gulp');
-var concat = require('gulp-concat')
-var uglify = require('gulp-uglify')
-var gzip = require('gulp-gzip')
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var gzip = require('gulp-gzip');
+var inject = require('gulp-inject');
+var sass = require('gulp-sass');
+var rev = require('gulp-rev');
+var fingerprint = require('gulp-fingerprint');
+var revNapkin = require('gulp-rev-napkin');
 
 gulp.task('html', function () {
-  return gulp.src('index.html')
+  var target = gulp.src('index.html')
+  var sources = gulp.src(['dist/style.css', 'dist/*.js'], {read: false});
+  return target.pipe(inject(sources, {ignorePath: 'dist'}))
     .pipe(gulp.dest('dist'))
 })
 
 gulp.task('js', function () {
   return gulp.src('js/*js')
-    .pipe(concat('all.js'))
+    .pipe(concat('all.min.js'))
     .pipe(uglify())
-    .pipe(gzip())
+    // .pipe(gzip())
+    .pipe(rev())
+    .pipe(fingerprint())
     .pipe(gulp.dest('dist'))
+    // .pipe(revNapkin())
+    .pipe(rev.manifest())
+    .pipe(gulp.dest('dist'));
 })
 
-gulp.task('styles', function () {
-  return gulp.src('css/stylesheets/style.css')
-    .pipe(gulp.dest('dist'))
+var fs = require('fs')
+
+gulp.task('jstest', function () {
+    return gulp.src('js/*.js')
+        .pipe(concat('all.min.js'))
+        .pipe(rev())
+        .pipe(gulp.dest('build'))
+        .pipe(rev.manifest())
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('fingerprint', ['jstest'], function () {
+  var manifest = JSON.parse(fs.readFileSync('build/rev-manifest.json', 'utf-8'))
+  return gulp.src('index.html')
+    .pipe(fingerprint(manifest))
+    .pipe(gulp.dest('build'));
 })
 
 gulp.task('assets', function () {
@@ -31,12 +56,18 @@ gulp.task('projects', function () {
     .pipe(gulp.dest('dist/assets/projects'))
 })
 
+gulp.task('sass', function () {
+  gulp.src('./sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist'));
+});
+
 gulp.task('watchout', function () {
-  gulp.watch('js/*.js', ['js'])
-  gulp.watch('css/*.css', ['styles'])
-  gulp.watch('index.html', ['html'])
+  gulp.watch('sass/*.scss', ['sass']);
+  gulp.watch('js/*.js', ['js', 'html'])
   gulp.watch('css/assets/*', ['assets'])
   gulp.watch('css/assets/projects', ['projects'])
+  gulp.watch('index.html', ['html'])
 })
 
-gulp.task('default', ['html', 'js', 'styles','assets', 'projects', 'watchout'])
+gulp.task('default', ['js','assets', 'sass', 'projects','html', 'watchout'])
