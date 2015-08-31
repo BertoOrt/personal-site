8b091f63ec0b6579c1d4e5fa14605ae28222c296
Roberto Ortega <berto.ort@gmail.com>
2015-08-31 13:57:45 -0600

    gulped

--- gulpfile.js
+++ gulpfile.js
@@ -7,43 +7,39 @@ var sass = require('gulp-sass');
 var rev = require('gulp-rev');
 var fingerprint = require('gulp-fingerprint');
 var revNapkin = require('gulp-rev-napkin');
+var fs = require('fs')
 
-gulp.task('html', function () {
-  var target = gulp.src('index.html')
-  var sources = gulp.src(['dist/style.css', 'dist/*.js'], {read: false});
-  return target.pipe(inject(sources, {ignorePath: 'dist'}))
-    .pipe(gulp.dest('dist'))
-})
+// gulp.task('html', function () {
+//   var target = gulp.src('index.html')
+//   var sources = gulp.src(['dist/style.css', 'dist/*.js'], {read: false});
+//   return target.pipe(inject(sources, {ignorePath: 'dist'}))
+//     .pipe(gulp.dest('dist'))
+// })
 
 gulp.task('js', function () {
   return gulp.src('js/*js')
     .pipe(concat('all.min.js'))
     .pipe(uglify())
-    // .pipe(gzip())
     .pipe(rev())
-    .pipe(fingerprint())
     .pipe(gulp.dest('dist'))
-    // .pipe(revNapkin())
     .pipe(rev.manifest())
     .pipe(gulp.dest('dist'));
 })
 
-var fs = require('fs')
+// gulp.task('jstest', function () {
+//     return gulp.src('js/*.js')
+//         .pipe(concat('all.min.js'))
+//         .pipe(rev())
+//         .pipe(gulp.dest('build'))
+//         .pipe(rev.manifest())
+//         .pipe(gulp.dest('build'));
+// });
 
-gulp.task('jstest', function () {
-    return gulp.src('js/*.js')
-        .pipe(concat('all.min.js'))
-        .pipe(rev())
-        .pipe(gulp.dest('build'))
-        .pipe(rev.manifest())
-        .pipe(gulp.dest('build'));
-});
-
-gulp.task('fingerprint', ['jstest'], function () {
-  var manifest = JSON.parse(fs.readFileSync('build/rev-manifest.json', 'utf-8'))
+gulp.task('fingerprint', ['js'], function () {
+  var manifest = JSON.parse(fs.readFileSync('dist/rev-manifest.json', 'utf-8'))
   return gulp.src('index.html')
     .pipe(fingerprint(manifest))
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest('dist'));
 })
 
 gulp.task('assets', function () {
@@ -64,10 +60,9 @@ gulp.task('sass', function () {
 
 gulp.task('watchout', function () {
   gulp.watch('sass/*.scss', ['sass']);
-  gulp.watch('js/*.js', ['js', 'html'])
-  gulp.watch('css/assets/*', ['assets'])
-  gulp.watch('css/assets/projects', ['projects'])
-  gulp.watch('index.html', ['html'])
+  gulp.watch('css/assets/*', ['assets']);
+  gulp.watch('js/*', ['fingerprint']);
+  gulp.watch('css/assets/projects', ['projects']);
 })
 
-gulp.task('default', ['js','assets', 'sass', 'projects','html', 'watchout'])
+gulp.task('default', ['fingerprint','assets', 'sass', 'projects', 'watchout'])
